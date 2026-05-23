f5fa43cf5cb6ad1111db0a6899bb0f39bef3c202
Dhananjaya Yapa <dhananjayayapa99@gmail.com>
2026-05-23 21:57:22 +0530

    chore: future-initial-file-setup

---
+++ client/src/components/profile/index.ts
@@ -0,0 +1,2 @@
+export * from "./PasswordChange";
+export * from "./ProfileInformation";
---
+++ client/src/redux/thunks/index.ts
@@ -0,0 +1,4 @@
+// Async thunks — each thunk wraps a service call and dispatches slice state changes
+export * from "./auth.thunks";
+export * from "./issue.thunks";
+export * from "./user.thunks";
